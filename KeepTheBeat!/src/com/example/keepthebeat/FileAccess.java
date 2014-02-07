5f229d44dc7713bf3f67a053560b50d8f83c1270
Florian CLAVEL <florian.clavel@live.Fr>
2014-02-07 11:36:33 +0100

    Add file write working

--- KeepTheBeat!/src/com/example/keepthebeat/FileAccess.java
+++ KeepTheBeat!/src/com/example/keepthebeat/FileAccess.java
@@ -1,7 +1,6 @@
 package com.example.keepthebeat;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,64 +10,26 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import android.content.Context;
-
 import android.graphics.AvoidXfermode.Mode;
 import android.util.Log;
 import android.widget.Toast;
 
-
 public class FileAccess {
-	public void writeToFile(Context context, String data) {
-	    String filename = "mysecondfile";
-	    String outputString = "Hello world!";
-
+	public void writeToFile(Context context, String fileName, String data) {
 	    try {
-	        File outFile = new File("/test/filename.text");
-	        if (outFile.getParentFile().mkdirs()) {
-	            outFile.createNewFile();
-	            FileOutputStream fos = new FileOutputStream(outFile);
-
-	            fos.write(outputString.getBytes());
-	            fos.flush();
-	            fos.close();
-	            Game.log(this, "Create file here : " + outFile.getAbsolutePath());
-	        }
-	        else {
-	            Game.log(this, "Folder doesnt exist");
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
+	        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_WORLD_READABLE));
+	        Game.log("", context.getFileStreamPath(fileName));
+	        outputStreamWriter.write(data);
+	        outputStreamWriter.close();
 	    }
+	    catch (IOException e) {
+	        Log.e("Exception", "File write failed: " + e.toString());
+	    } 
 	}
 
 
 	private String readFromFile(Context context) {
 
-	    String ret = "";
-
-	    try {
-	        InputStream inputStream = context.openFileInput("/config.txt");
-	        Game.log("", context.getFileStreamPath("/config.txt"));
-	        if ( inputStream != null ) {
-	            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-	            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-	            String receiveString = "";
-	            StringBuilder stringBuilder = new StringBuilder();
-
-	            while ( (receiveString = bufferedReader.readLine()) != null ) {
-	                stringBuilder.append(receiveString);
-	            }
-
-	            inputStream.close();
-	            ret = stringBuilder.toString();
-	        }
-	    }
-	    catch (FileNotFoundException e) {
-	        Log.e("login activity", "File not found: " + e.toString());
-	    } catch (IOException e) {
-	        Log.e("login activity", "Can not read file: " + e.toString());
-	    }
-
-	    return ret;
+	   return "";
 	}
 }
