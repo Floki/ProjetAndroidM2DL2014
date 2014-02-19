02ff9e9fa4bb16ea1601dc8fe3e1b7ac212cc438
Florian CLAVEL <florian.clavel@live.Fr>
2014-02-19 23:03:53 +0100

    Score in title activity now displaying

--- KeepTheBeat!/src/com/example/keepthebeat/title/Title.java
+++ KeepTheBeat!/src/com/example/keepthebeat/title/Title.java
@@ -5,11 +5,15 @@ import java.io.File;
 import com.example.keepthebeat.CustomActivity;
 import com.example.keepthebeat.R;
 import com.example.keepthebeat.ScoreActivity;
+import com.example.keepthebeat.db.ScoreContract.ScoreEntry;
+import com.example.keepthebeat.db.ScoreDbHelper;
 import com.example.keepthebeat.game.Game;
 import com.example.keepthebeat.utils.Constants;
 import com.example.keepthebeat.utils.Tools;
 
 import android.content.Intent;
+import android.database.Cursor;
+
 import com.example.keepthebeat.parameters.Parameters;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,6 +22,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Title extends CustomActivity {
@@ -69,6 +74,28 @@ public class Title extends CustomActivity {
 				finish();
 			}
 		});
+		
+		/*
+		 * Fill up best score & track
+		 */
+		ScoreDbHelper scoreDbHelper = new ScoreDbHelper(this);
+		Cursor c = scoreDbHelper.getBestScore();
+
+		String bestScore = "";
+		String bestSong = "not yet played";
+
+		if( c.moveToFirst() ) {
+			bestScore = c.getInt( c.getColumnIndex( ScoreEntry.COLUMN_NAME_SCORE) )+"";
+			( (TextView) findViewById(R.id.bestScoreLabel) ).setVisibility( TextView.VISIBLE );
+		}
+		else {
+			( (TextView) findViewById(R.id.bestScoreLabel) ).setVisibility( TextView.INVISIBLE );
+		}
+
+		c.close();
+		scoreDbHelper.closeDb();
+
+		( (TextView) findViewById(R.id.bestScoreValue) ).setText( bestScore );
 	}
 
 	@Override
